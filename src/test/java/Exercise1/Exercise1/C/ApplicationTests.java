18f035e2e95be9eac15b4518c56586914d4fca1a
cindynrh <cindynurhabibah@gmail.com>
2022-10-17 23:48:18 +0700

    add database

---
+++ src/test/java/Exercise1/Exercise1/C/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Exercise1.Exercise1.C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
